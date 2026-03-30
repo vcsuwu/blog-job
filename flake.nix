e7e8ad2c33bcb365f5de4e98bbd2b6b14c362ff6
hollow <placeholder>
2026-03-30 10:30:50 +0500

    initial commit of my simple blog app

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      # system should match the system you are running on
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "hola"
+          '';
+        };
+    };
+}
